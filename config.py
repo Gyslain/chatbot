57053699f88105167aa9439b8c7f1f24b2b1aacc
Ghislain Viguier <ghislain.viguier@gmail.com>
2022-02-14 17:24:12 +0100

    Remove yml files

--- config.py
+++ config.py
@@ -2,19 +2,34 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 """Configuration for the bot."""
+import os
 
-from dotenv import dotenv_values
+# from dotenv import dotenv_values
 
-config = dotenv_values(".env")
+# config = dotenv_values(".env")
 
 
 class DefaultConfig:
     """Configuration for the bot."""
 
-    PORT = 3978
-    APP_ID = config["APP_ID"]
-    APP_PASSWORD = config["APP_PASSWORD"]
-    LUIS_APP_ID = config["LUIS_APP_ID"]
-    LUIS_API_KEY = config["LUIS_AUTHORING_KEY"]
-    LUIS_END_POINT = config["LUIS_AUTHORING_END_POINT"]
-    APPINSIGHTS_INSTRUMENTATION_KEY = config["APPINSIGHTS_INSTRUMENTATION_KEY"]
+    PORT = 8000
+
+    APP_ID = os.environ.get("APP_ID", "")
+    APP_PASSWORD = os.environ.get("APP_PASSWORD", "")
+
+    LUIS_APP_ID = os.environ.get("LUIS_APP_ID", "")
+    LUIS_API_KEY = os.environ.get("LUIS_AUTHORING_KEY", "")
+    LUIS_END_POINT = os.environ.get("LUIS_AUTHORING_END_POINT", "")
+    
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
+        "APPINSIGHTS_INSTRUMENTATION_KEY", ""
+    )
+
+
+#     PORT = 3978
+#     APP_ID = config["APP_ID"]
+#     APP_PASSWORD = config["APP_PASSWORD"]
+#     LUIS_APP_ID = config["LUIS_APP_ID"]
+#     LUIS_API_KEY = config["LUIS_AUTHORING_KEY"]
+#     LUIS_END_POINT = config["LUIS_AUTHORING_END_POINT"]
+#     APPINSIGHTS_INSTRUMENTATION_KEY = config["APPINSIGHTS_INSTRUMENTATION_KEY"]
